61747d39ef4b428188846bbc86f83c2b2fbe6975
Aji5ai <m1ck3ym0u5e@live.fr>
2023-05-01 16:28:42 +0200

    Version finale au 01-05-23

--- CV.js
+++ CV.js
@@ -1,5 +1,6 @@
 const menuTexte = document.getElementById("menu_mobile"); //on reprend la liste du menu
 
+/* Fonction pour afficher le menu mobile quand on clique sur MENU et le hamburger, et pour le cacher une fois que l'utilisateur a cliqué sur un des liens */
 function toggleMenu() {
 if(menuTexte.style.display == "flex") { // si le menu est visible, on le cache
     menuTexte.style.display = "none";
